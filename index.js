3617ba873a87c4321cddb3797f6bbaa44ff00c9a
Gloria Lim <glorialimartt@gmail.com>
2023-10-20 23:56:32 -0400

    Implement fetchISSFlyOverTimes function and add error handling

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ fetchCoordsByIP('72.136.1.19', (error, coordinates) => {
   console.log("It worked! Returned coordinates: ", coordinates);
 });
 
-const coords =  { latitude: 32.7766642, longitude: -96.7969879 }
+//test
+// const coords = { latitude: '32.7766642', longitude: '-96.7969879' }
+// const coords = { latitude: '12', longitude: 'a' }
+const coords =  { latitude: 43.467517, longitude: -79.6876659 };
 fetchISSFlyOverTimes(coords, (error, output) => {
   if (error) {
-    console.log("It didn't work! Check coordinates")
+    console.log("It didn't work! Error: ", error);
     return;
   }
-  console.log("It worked!", output)
-})
+  console.log("It worked!");
+  console.log(output);
+});
--- iss.js
+++ iss.js
@@ -1,9 +1,8 @@
-const { parse } = require('path');
 const request = require('request');
 const api = 'https://api.ipify.org?format=json';
 const geoAPIEndpoint = "http://ipwho.is/";
 
-const issAPI = 'https://iss-flyover.herokuapp.com/json/?lat=YOUR_LAT_INPUT_HERE&lon=YOUR_LON_INPUT_HERE'
+const issAPI = 'https://iss-flyover.herokuapp.com/json/';
 
 
 const fetchMyIP = function(callback) {
@@ -42,8 +41,38 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-const fetchISSFlyOverTimes = function (coords, callback) {
-  
-}
+// ?lat=YOUR_LAT_INPUT_HERE&lon=YOUR_LON_INPUT_HERE
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const issAPIQuery = `${issAPI}?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(issAPIQuery, (error, response, body) => {
+    const parsedBody = JSON.parse(body);
+    
+    if (error) return callback(error, null);
+
+    if (!(response.statusCode >= 200 || response.statusCode < 300)) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    if (parsedBody === "invalid coordinates") {
+      const message = "Invalid coordinates!";
+      return callback(Error(message), null);
+    }
+
+    if (isNaN(coords.latitude)) {
+      const message = "Invalid latitude";
+      return callback(Error(message), null);
+    }
+    
+    if (isNaN(coords.longitude)) {
+      const message = "Invalid longitude";
+      return callback(Error(message), null);
+    }
+
+    const flyOverTimesArray = parsedBody["response"];
+    callback(error, flyOverTimesArray);
+  });
+};
 
 module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
